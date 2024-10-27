b40d41460183fd638a0fe725224ea3f9e31ab931
Deltajoestar04 <144274786+Deltajoestar04@users.noreply.github.com>
2024-10-27 13:56:52 -0700

    Update Bisciesto.js

--- Bisciesto.js
+++ Bisciesto.js
@@ -51,21 +51,4 @@ function busqueda(){
     }
 
 
-=======
-function ejecutar(){
-  
-    var valor1= document.getElementById("valor1").value;
-    var valor2= document.getElementById("valor2").value;
-    
-    var div_elementos = document.getElementById("elementos");
-
-    for(let i = valor1; i<= valor2; i++){
-        if (i % 4 == 0) {
-            div_elementos.innerHTML += '<p style="color: red; font-weight: bold;">' + i + ' (Bisiesto)</p>';
-        } else {
-            div_elementos.innerHTML += '<p>' + i + '</p>';
-        }
-
-    }
-
 }
