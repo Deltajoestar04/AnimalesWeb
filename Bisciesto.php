4c0a662976b15aba5396c2b4e42904c5d9bde73a
Deltajoestar04 <144274786+Deltajoestar04@users.noreply.github.com>
2024-10-27 13:55:54 -0700

    Update Bisciesto.php

--- Bisciesto.php
+++ Bisciesto.php
@@ -19,21 +19,3 @@
 
         </div>
     <?php include("footer.php")?>
-=======
-<?php include ("header.php")?>
-<script src="Bisciesto.js"></script>
-<link rel="stylesheet" href="css/styles.css" type="text/css">
-
-        <div class="conteiner">
-        <h1>Ejemplo java</h1>
-        <h2>Años Bisciestos</h2>
-        <h2>Año de inicio / Año final</h2>
-
-           <input type="number" id="valor1"></input> 
-            <input type="number" id="valor2"></input>
-             <button onclick="ejecutar()">ejecutar</button>
-            
-            <div id="elementos"></div>
-
-        </div>
-    <?php include("footer.php")?>
