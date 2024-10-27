19fd03150ef1e0899823fceccf2c8418dbb4c242
Deltajoestar04 <144274786+Deltajoestar04@users.noreply.github.com>
2024-10-27 13:50:23 -0700

    exam

--- Bisciesto.js
+++ Bisciesto.js
@@ -1,3 +1,58 @@
+<<<<<<< HEAD
+function ejecutar(){
+  
+    var valor1= document.getElementById("valor1").value;
+    var valor2= document.getElementById("valor2").value;
+    
+    var div_elementos = document.getElementById("elementos");
+    div_elementos.innerHTML = '';
+
+
+    if(valor2<valor1){
+        alert("el año final debe ser mayor al año inicio")
+    }else for(let i = valor1; i<= valor2; i++){{
+    
+        if (i % 4 == 0) {
+            div_elementos.innerHTML += '<p style="color: red; font-weight: bold;">' + i + ' (Bisiesto)</p>';
+        } else {
+            div_elementos.innerHTML += '<p>' + i + '</p>';
+        }
+
+        }
+
+    }
+}
+
+function total() {
+    var valor1 = parseInt(document.getElementById("valor1").value);
+    var valor2 = parseInt(document.getElementById("valor2").value);
+    var totalf = 0;
+
+    for (let i = valor1; i <= valor2; i++) {
+        if (i % 4 == 0) {
+            totalf += 1;
+        }
+    }
+
+    alert("Hay " + totalf + " año/s bisiestos en la lista");
+}
+
+function busqueda(){
+    var busqueda= document.getElementById("busqueda").value;
+    
+    var div_elementos = document.getElementById("elementos");
+    div_elementos.innerHTML = '';
+
+    if (busqueda % 4 == 0) {
+        div_elementos.innerHTML += '<p style="color: red; font-weight: bold;">El año ' + busqueda + ' es/fue Bisiesto :D</p>';
+ 
+    }else{
+        div_elementos.innerHTML += '<p style="color: blue;font-weight: bold;">El año ' + busqueda + ' no es/fue Bisiesto D:</p>';
+
+    }
+
+
+=======
 function ejecutar(){
   
     var valor1= document.getElementById("valor1").value;
@@ -14,4 +69,5 @@ function ejecutar(){
 
     }
 
+>>>>>>> 5b6a1c9149442b07f80fdaf5bb1fe4f83ba333ec
 }
--- Bisciesto.php
+++ Bisciesto.php
@@ -1,3 +1,26 @@
+<<<<<<< HEAD
+<?php include ("header.php")?>
+<script src="Bisciesto.js"></script>
+<link rel="stylesheet" href="css/styles.css" type="text/css">
+
+        <div class="conteiner">
+        <h1>Años Bisiestos</h1>
+        <h2>Año de inicio / Año final</h2>     
+           <input type="number" id="valor1"></input> 
+            <input type="number" id="valor2"></input>
+            <button onclick="ejecutar()">ejecutar</button>
+            <button onclick="total()">Total</button>
+            <button onclick="busqueda()">Busqueda</button>
+            
+            <input type="number" id="busqueda"></input>
+
+             
+            
+            <div id="elementos"></div>
+
+        </div>
+    <?php include("footer.php")?>
+=======
 <?php include ("header.php")?>
 <script src="Bisciesto.js"></script>
 <link rel="stylesheet" href="css/styles.css" type="text/css">
@@ -15,3 +38,4 @@
 
         </div>
     <?php include("footer.php")?>
+>>>>>>> 5b6a1c9149442b07f80fdaf5bb1fe4f83ba333ec
